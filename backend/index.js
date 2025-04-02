c261598e83c80ddb846341567e3aa714d0cb8651
Your Name <leonard.breu@gmail.com>
2025-04-02 15:11:36 +0200

    functioning server connection

---
+++ backend/index.js
@@ -0,0 +1,24 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+
+import { connectDB } from "./lib/db.js";
+import { app, server } from "./lib/socket.js";
+
+
+dotenv.config();
+
+const PORT = process.env.PORT;
+
+app.use(express.json());
+
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+}));
+
+
+server.listen(PORT, () => {
+    console.log("server is running on PORT:" + PORT);
+    connectDB();
+});
